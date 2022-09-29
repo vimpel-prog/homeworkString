82cb45daf2e84c7bc2812453004eab924dda4e9d
sergey <vimpel812@gmail.com>
2022-09-30 02:16:12 +0300

    HW is Ready

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+    }
+
+    public static void task1() {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника — "+fullName);
+    }
+    public static void task2() {
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — "+fullName.toUpperCase());
+    }
+
+    public static void task3() {
+        String fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё","е");
+        System.out.println("Данные ФИО сотрудника — "+fullName);
     }
 }
